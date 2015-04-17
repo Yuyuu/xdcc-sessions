9da9e14281f593975a63fc1c220db644755654f4
Vincent Tertre <vincent.tertre@gmail.com>
2015-04-17 15:45:49 +0200

    Exclude some files from coverage

--- tasks/istanbul.js
+++ tasks/istanbul.js
@@ -5,10 +5,18 @@ var spawnSync = require("child_process").spawnSync;
 module.exports = function (grunt) {
   var binDir = "node_modules/.bin";
 
+  var excludedPatterns = [
+    "app.js",
+    "spec/**",
+    "src/web/server.js",
+    "src/web/router.js",
+    "src/infrastructure/mongo/database.js"
+  ];
+
   grunt.registerTask("istanbul", function () {
     spawnSync(
       binDir + "/istanbul",
-      ["cover", binDir + "/_mocha", "--", "--reporter", "spec", "--recursive", "spec"],
+      ["cover", "-x", excludedPatterns.join(" "), binDir + "/_mocha", "--", "--reporter", "spec", "--recursive", "spec"],
       {stdio: "inherit", encoding: "UTF-8"}
     );
   });
